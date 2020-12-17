1b5f572c851e47a7e16bf362901c4fa16262bfe6
Paweł <kalicki.paul@icloud.com>
2020-12-17 12:07:05 +0100

    :sparkles: Prepared class for exercise and research

--- laboratories/introduction.py
+++ laboratories/introduction.py
@@ -1,4 +1,11 @@
 class MLP:
+    activation, optimizer = '', ''
+    epochs, bs = 0, 0
+    lr = 0.0
 
-    def __init__(self):
-        pass
+    def __init__(self, activation='softmax', epochs=10, optimizer='sgd', learning_rate=0.01, bs=256):
+        self.activation = activation
+        self.epochs = epochs
+        self.optimizer = optimizer
+        self.lr = learning_rate
+        self.bs = bs
--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from laboratories import MLP
 
 if __name__ == "__main__":
+    activation = ['sigmoid', 'hard_sigmoid', 'tanh', 'linear', 'relu', 'softmax']
+    epochs = [10, 100, 1000]
+    optimizers = ['adam', 'sgd', 'adadelta', 'adagrad', 'rmsprop']
+    learning_rate = 0.01
     lab1 = MLP()
