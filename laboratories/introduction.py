2544d962915f5b23caac231dfb7f18c0299873d9
Paweł <kalicki.paul@icloud.com>
2020-12-17 12:11:30 +0100

    :sparkles: Added model generation

--- laboratories/introduction.py
+++ laboratories/introduction.py
@@ -1,5 +1,8 @@
 from keras.datasets import mnist
 from keras.utils import to_categorical
+from keras.layers import Flatten, Dense
+from keras.models import Sequential
+from keras.optimizers import SGD, Adam, Adadelta, Adagrad, RMSprop
 
 
 class MLP:
@@ -9,6 +12,7 @@ class MLP:
     trainX, trainY = [], []
     testX, testY = [], []
     trainNorm, testNorm = [], []
+    model = None
 
     def __init__(self, activation='softmax', epochs=10, optimizer='sgd', learning_rate=0.01, bs=256):
         self.activation = activation
@@ -19,6 +23,7 @@ class MLP:
 
         self.trainX, self.trainY, self.testX, self.testY = self.load_dataset()
         self.trainNorm, self.testNorm = self.normalize_data('x')
+        self.model = self.get_model()
 
     @staticmethod
     def load_dataset():
@@ -44,3 +49,21 @@ class MLP:
             print("Probably this is wrong set")
         train, test = train / 255.0, test / 255.0
         return train, test
+
+    def get_model(self):
+        model = Sequential()
+        model.add(Flatten())
+        model.add(Dense(10, activation=self.activation, input_shape=(28 * 28, 1)))
+        opt = None
+        if self.optimizer == 'adam':
+            opt = Adam(lr=self.lr, momentum=0.9)
+        elif self.optimizer == 'sgd':
+            opt = SGD(lr=self.lr, momentum=0.9)
+        elif self.optimizer == 'adadelta':
+            opt = Adadelta(lr=self.lr, momentum=0.9)
+        elif self.optimizer == 'adagrad':
+            opt = Adagrad(lr=self.lr, momentum=0.9)
+        elif self.optimizer == 'rmsprop':
+            opt = RMSprop(lr=self.lr, momentum=0.9)
+        model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+        return model
